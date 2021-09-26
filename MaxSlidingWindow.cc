6328cce074ce98ae6256acca49d5f6b8e4856d8b
yong <yong@blueoceanpay.com>
2021-09-26 08:38:56 +0800

    滑动最大窗口

---
+++ MaxSlidingWindow.cc
@@ -0,0 +1,26 @@
+#include "stdc++.h"
+
+using namespace std;
+class MaxSlidingWindow {
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // 删除那些永远不能成为最优解的那些数
+        // 下标时间 递增 值递减的队列
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            // 删除出界的选项
+            while (!q.empty() && q.front() <= i - k)
+                q.pop_front();
+            // 插入新选项 i 维护单调性
+            while (!q.empty() && nums[q.front()] < nums[i])
+                q.pop_back();
+            q.push_back(i);
+            if (i >= k - 1) {
+                ans.push_back(nums[q.front()]);
+            }
+        }
+        return ans;
+    }
+
+private:
+    deque<int> q; // 存储下标
+}
